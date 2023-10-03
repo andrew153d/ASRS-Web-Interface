3277fb3075246c267011a8589a92ea0bcedd918d
andrew153d <andrewwisnergrow@gmail.com>
2023-10-03 18:19:00 -0400

    updates

--- static/script.js
+++ static/script.js
@@ -1,21 +1,3 @@
-function tableElementMouseOver(element){
-    const descendantButtons = element.querySelectorAll('a');
-
-    // Loop through each descendant button and apply the expandDiv function
-    descendantButtons.forEach(function(button) {
-      //button.style.display = "flex";
-    });
-}
-
-function tableElementMouseLeave(element){
-    const descendantButtons = element.querySelectorAll('a');
-
-    // Loop through each descendant button and apply the expandDiv function
-    descendantButtons.forEach(function(button) {
-      //button.style.display = "none";
-    });
-}
-
 function reloadPage() {
     location.reload();
   }
